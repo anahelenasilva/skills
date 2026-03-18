10c0dbc30f16a5ea6ef838d06b02c37e53235492
Ana Helena <ana.dasilva@withclutch.com>
2026-03-18 18:19:45 -0300

    chore: add statusline script

---
+++ statusline.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# Claude Code status line: model | cost | context % | duration
+input=$(cat)
+
+pct=$(echo "$input" | jq -r '.context_window.used_percentage // 0' | awk '{printf "%.0f", $1}')
+used=$(echo "$input" | jq -r '.context_window.used_tokens // 0')
+total=$(echo "$input" | jq -r '.context_window.total_tokens // 0')
+
+# Format token counts as K
+used_k=$(echo "$used" | awk '{printf "%.0fK", $1/1000}')
+total_k=$(echo "$total" | awk '{printf "%.0fK", $1/1000}')
+
+dur_ms=$(echo "$input" | jq -r '.cost.total_duration_ms // 0')
+dur_s=$((dur_ms / 1000))
+mins=$((dur_s / 60))
+secs=$((dur_s % 60))
+
+printf "%s %s/%s (%s%%) ctx | %dm%02ds" "$cost" "$used_k" "$total_k" "$pct" "$mins" "$secs"
