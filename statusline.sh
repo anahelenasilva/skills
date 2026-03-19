047361490d13a816743609be0050e2127410b640
Ana Helena <ana.dasilva@withclutch.com>
2026-03-19 16:16:50 -0300

    chore: update statusline script

--- statusline.sh
+++ statusline.sh
@@ -3,8 +3,8 @@
 input=$(cat)
 
 pct=$(echo "$input" | jq -r '.context_window.used_percentage // 0' | awk '{printf "%.0f", $1}')
-used=$(echo "$input" | jq -r '.context_window.used_tokens // 0')
-total=$(echo "$input" | jq -r '.context_window.total_tokens // 0')
+used=$(echo "$input" | jq -r '((.context_window.total_input_tokens // 0) + (.context_window.total_output_tokens // 0))')
+total=$(echo "$input" | jq -r '.context_window.context_window_size // 0')
 
 # Format token counts as K
 used_k=$(echo "$used" | awk '{printf "%.0fK", $1/1000}')
@@ -15,4 +15,4 @@ dur_s=$((dur_ms / 1000))
 mins=$((dur_s / 60))
 secs=$((dur_s % 60))
 
-printf "%s %s/%s (%s%%) ctx | %dm%02ds" "$cost" "$used_k" "$total_k" "$pct" "$mins" "$secs"
+printf "%s/%s (%s%%) ctx | %dm%02ds" "$used_k" "$total_k" "$pct" "$mins" "$secs"
